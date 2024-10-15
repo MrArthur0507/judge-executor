a58b5eed5b66c713d9600b80ee7a69fe74d6abc9
Bozhidar <mrarthur0507@gmail.com>
2024-10-15 11:30:28 +0300

    simple controller for receiving submissions and separate project for models

---
+++ Executor.Api/Controllers/ExecuteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Executor.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExecuteController : ControllerBase
+    {
+    }
+}
---
+++ Executor.Models/Submissions/Submission.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Executor.Models.Submissions
+{
+    public class Submission
+    {
+        public string Code { get; set; }
+        public string Language { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
