d1ae47852462dd5b36f0840588be6fec06f683cd
Bozhidar <mrarthur0507@gmail.com>
2024-11-13 17:09:57 +0200

    added test cases dtos

---
+++ Executor.Models/Dtos/Creational/CreateTestCaseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Executor.Models.Dtos.Creational
+{
+    public class CreateTestCaseDto
+    {
+        public Guid ProblemId { get; set; }
+        public string Input { get; set; }
+        public string ExpectedOutput { get; set; }
+        public bool IsZeroCase { get; set; }
+    }
+}
